c83b629625d0b1861d79a60978c9c1d3b20cefd3
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2025-12-29 02:49:29 -0500

    make gros mike spawnable by crucible rotarot

--- lib/vanillatweaks.lua
+++ lib/vanillatweaks.lua
@@ -1,8 +1,12 @@
+SMODS.Joker:take_ownership("gros_michel", {
+    biblio_can_crucible = true
+}, false)
+
 SMODS.Joker:take_ownership("cavendish", {
     set_card_type_badge = function (self, card, badges)
         badges[1] = create_badge(localize("k_biblio_evolved", "labels"), SMODS.Gradients.biblio_evolved, nil, 1.2)
     end
-}, true)
+}, false)
 
 SMODS.Consumable:take_ownership("wheel_of_fortune", {
     name = "Wheel of Fortune (Bibliography'd)",
