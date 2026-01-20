c89d07acfde9e059c9ee78f40c6529e57ad073bb
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2026-01-20 00:51:52 -0500

    lsp defs: programming for your programming

--- lsp_defs.lua
+++ lsp_defs.lua
@@ -14,13 +14,20 @@ function HEX(hex) end
 ---@field type string What the credit is for
 ---@field credit string Who's being credited
 
----@class BibCreditBadge
----@field type? string What the credit is for
----@field credit? string Who's being credited
----@field credits? BibCredit[] List of credits for a multi-credit badge
+---@class BibCreditBadgeSingle
+---@field type string What the credit is for
+---@field credit string Who's being credited
 ---@field bcol? ColorRGBA Background color as formatted by `HEX()`; default #ca7ca7
 ---@field tcol? ColorRGBA Text color; default #fff
 ---@field scale? number Scale (default 0.7 to standard badges' 1)
+
+---@class BibCreditBadgeMulti
+---@field credits BibCredit[] List of credits for a multi-credit badge
+---@field bcol? ColorRGBA Background color as formatted by `HEX()`; default #ca7ca7
+---@field tcol? ColorRGBA Text color; default #fff
+---@field scale? number Scale (default 0.7 to standard badges' 1)
+
+---@alias BibCreditBadge BibCreditBadgeSingle | BibCreditBadgeMulti
 
 ---@class SMODS.Center
 ---@field biblio_evolution? (string | string[] | fun(self:SMODS.Joker|table, card:Card|table, crucible:Card|table):string) Key of center to evolve into, or table of such keys to be selected randomly, or function which returns such a key
