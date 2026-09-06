cbb894b24822447587a42a8a9d503527d310f770
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2026-09-05 20:25:50 -0400

    truly, smods is a shining example of confusing multiple negatives. and/or i need to get more sleep

--- tags/starter.lua
+++ tags/starter.lua
@@ -8,7 +8,7 @@ SMODS.Tag{
     in_pool = function (self, args)
         return false
     end,
-    in_collection = false,
+    no_collection = true,
     loc_vars = function (self, info_queue, tag)
         info_queue[#info_queue+1] = G.P_CENTERS.p_biblio_starter
     end,
